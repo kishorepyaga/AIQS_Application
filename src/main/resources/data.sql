1eabeedd3ac664faa2ddd6c94a4081e666ec1762
Kishore Pyaga <133911201+kishorepyaga@users.noreply.github.com>
2023-10-15 11:56:55 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,7 +12,6 @@ insert into insurance_provider (insurance_provider_name, discount, insurance_dec
 
 
 -- User Profile Default Insertions
-
 insert into user_profile (user_id, user_name, role, dob, profession, contact_number, email_id, gender, password, permanent_address, present_address) values
     (10001, 'Admin', 'ROLE_ADMIN', '1999-01-01', 'Maintaining', 9999999999, 'admin@gmail.com', 'male', 'admin', null, null),
     (10002, 'User', 'ROLE_USER', '2001-02-15', 'Browsing', 8888888888, 'user@gmail.com', 'male', 'user', 'NoWhere', 'EveryWhere'),
@@ -21,28 +20,23 @@ insert into user_profile (user_id, user_name, role, dob, profession, contact_num
 
 
 -- Credit Card Default Insertions
-
 insert into credit_card values (
 	select user_id from user_profile where user_id = 10003, curdate(), curdate()+100, 50000, 1000000001, 'ICICI');
 
 
 -- Vehicle Default Insertions
-
 insert into vehicle values (current_date(), null, 5, 500000, 450000, 101, 11111, 2023, 2023, 'Hyderabad', 'Petrol', 'Telangana', 'Harley Davidson', 'Forty Eight', 'Harley Davidson 48');
 
 -- Driver Default Insertions
-
 insert into driver values (12345, select user_id from user_profile where user_id = 10003, 
 	select vehicle_id from vehicle where vehicle_id = 101, select user_name from user_profile where user_id = 10003);
 
 
 -- Quote Default Insertions
-
 insert into quote values (12, curdate(), curdate()-1, 1001, curdate()+365, select vehicle_id from vehicle where vehicle_id=101, 
 	select credit_card_number from credit_card where credit_card_number = 1000000001, 
 		select insurance_provider_name from insurance_provider where insurance_provider_name = 'ABC', 'New', 'Valid');
 
 		
 -- Updating Vehicle quote_id from null to it's assigned quote
-
 update vehicle set quote_id = select quote_id from quote where vehicle_id = 101;
